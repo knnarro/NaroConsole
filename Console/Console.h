803a7392e729336c4f2791617c8691bdf4ff8a51
pireco <pireco@pirecoui-MacBookPro.local>
2022-11-29 06:45:17 +0900

    first commit

---
+++ Console/Console.h
@@ -0,0 +1,18 @@
+//
+//  Console.h
+//  Console
+//
+//  Created by pireco on 2022/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Console.
+FOUNDATION_EXPORT double ConsoleVersionNumber;
+
+//! Project version string for Console.
+FOUNDATION_EXPORT const unsigned char ConsoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Console/PublicHeader.h>
+
+
---
+++ Console/Console.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Console
+//
+//  Created by pireco on 2022/11/29.
+//
+
+import Foundation
+
+public struct Console {
+    public static func sayHello(){
+        print("Hello")
+    }
+}
